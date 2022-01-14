3cbc29693b179190df668b4e4a4bd2f7446f05e6
Craig <indigomx9@gmail.com>
2022-01-14 15:58:47 -0500

    Created the Add Component.

--- client/src/containers/Add.jsx
+++ client/src/containers/Add.jsx
@@ -1,10 +1,96 @@
 import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { BookAPI } from "../global/BookAPI";
 
 export const Add = () => {
+    const navigate = useNavigate();
+    const [book, setBook] = React.useState({
+        _id: "", title: "", first: "", 
+        last: "", age: 0, info: ""
+    });
+    const [addBook, { 
+        isLoading: updating, isSuccess: saved 
+    }] = BookAPI.useCreateMutation();
+
+    const handleChange = (event) => {
+        setBook({...book, 
+            [event.target.name]: event.target.value});
+    };
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        await addBook(book);
+        setBook({
+            _id: "", title: "", first: "", 
+            last: "", age: 0, info: ""
+        });
+        navigate("/");
+    };
+
     return (
         <React.Fragment>
-            <h1>App</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi suscipit commodi magni officiis enim debitis dolor omnis, expedita quas tempora repellendus dolorum tenetur, numquam consectetur esse sit ratione, laudantium hic?</p>
+            <h1>Create a new Book</h1>
+            <form onSubmit={handleSubmit}>
+                <aside>
+                    <label htmlFor="title">Title</label>
+                    <input 
+                        type="text" 
+                        name="title"
+                        placeholder="Title"
+                        value={book.title}
+                        onChange={handleChange}
+                    />
+                </aside>
+                <aside>
+                    <label htmlFor="first">First</label>
+                    <input 
+                        type="text" 
+                        name="first" 
+                        placeholder="First" 
+                        value={book.first}
+                        onChange={handleChange}
+                    />
+                </aside>
+                <aside>
+                    <label htmlFor="last">Last</label>
+                    <input 
+                        type="text"
+                        name="last"
+                        placeholder="Last"
+                        value={book.last}
+                        onChange={handleChange}
+                    />
+                </aside>
+                <aside>
+                    <label htmlFor="age">Age</label>
+                    <input 
+                        type="text" 
+                        name="age"
+                        placeholder="Age"
+                        value={book.age}
+                        onChange={handleChange}
+                    />
+                </aside>
+                <aside>
+                    <label htmlFor="info">Info</label>
+                    <input 
+                        type="text" 
+                        name="info"
+                        placeholder="Info"
+                        value={book.info}
+                        onChange={handleChange}
+                    />
+                </aside>
+                <footer>
+                    <button><Link to="/">Cancel</Link></button>
+                    <button type="submit"
+                        >{updating ? "Adding..." : "Add Book"}
+                    </button>
+                    {saved && (
+                        <aside>Book added.  redirecting...</aside>
+                    )}
+                </footer>
+            </form>
         </React.Fragment>
     );
 };
--- client/src/global/BookAPI.js
+++ client/src/global/BookAPI.js
@@ -9,6 +9,45 @@ export const BookAPI = createApi({
     endpoints: (builder) => ({
         fetchAll: builder.query({
             query: () => "/",
+            providesTags: (result) => result ?
+            [...result.map(({ _id }) =>
+                ({ type: "Books", _id })),
+                { type: "Books", id: "LIST" },
+            ] : [{ type: "Books", id: "LIST" }],
+        }),
+        getOne: builder.query({
+            query: (_id) => `/${_id}`,
+            providesTags: (result, error, _id) =>
+                [{ type: "Books", _id }],
+        }),
+        create: builder.mutation({
+            query: (book) => {
+                return {
+                    url: `/`,
+                    method: "POST",
+                    body: book
+                }
+            },
+            invalidatesTags: [{ type: "Books", id: "LIST" }],
+        }),
+        update: builder.mutation({
+            query: (_id, ...book) => {
+                return {
+                    url: `/${_id}`,
+                    method: "PUT",
+                    body: book
+                }
+            },
+            invalidatesTags: [{ type: "Books", id: "LIST" }],
+        }),
+        delete: builder.mutation({
+            query: (_id) => {
+                return {
+                    url: `/${_id}`,
+                    method: "DELETE",
+                }
+            },
+            invalidatesTags: [{ type: "Books", id: "LIST" }],
         })
     }),
 });
